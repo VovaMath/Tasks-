fb6d3bf1940d699c33974d5d1010e8b029ce1ea3
Vladimir <64377415+VovaMath@users.noreply.github.com>
2020-05-06 14:50:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-/* Написать программу: построить список L, вставить в начало - push(список, элемент),
-вывод на печать, вставка до заданного элемента, вставка после заданного элемента,
-вставка в хвост,  удалить первый элемент (элемент = pop(список) ),
-удалить последний элемент, удалить заданный элемент + индивидуальное задание.
 
-
-
-*/
 
 #include <iostream>
 #include <stdio.h>
@@ -21,7 +14,7 @@ list L;
 list M1;
 list M2;
 
-void L_print(list *L) { // вывод на экран
+void L_print(list *L) { // РІС‹РІРѕРґ РЅР° СЌРєСЂР°РЅ
     printf("List:[");
     for(;;) {
         printf("%d ", L->id);
@@ -30,41 +23,47 @@ void L_print(list *L) { // вывод на экран
     }
     printf("]\n");
 }
-void L_start(list *L, int v) { // L:=список из одного элемента v
+void L_start(list *L, int v) { // L:=СЃРїРёСЃРѕРє РёР· РѕРґРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р° v
     L->id = v;
     L->next = NULL;
 }
-void L_pop_front(list *L, int v) { // вставить число v в начало списка L
+void L_pop_front(list *L, int v) { // РІСЃС‚Р°РІРёС‚СЊ С‡РёСЃР»Рѕ v РІ РЅР°С‡Р°Р»Рѕ СЃРїРёСЃРєР° L
     link *L1 = new link;
     L1->id = L->id;
     L1->next = L->next;
     L->id = v;
     L->next = L1;
 }
-void L_pop(list *L, int i, int v) { // вставить число v перед номером i в списке L
+void L_pop(list *L, int i, int v) { // РІСЃС‚Р°РІРёС‚СЊ С‡РёСЃР»Рѕ v РїРµСЂРµРґ РЅРѕРјРµСЂРѕРј i РІ СЃРїРёСЃРєРµ L
     for( int j=0; j<i; j++) {
-        if( (L = L->next)==NULL) return;  // ошибка
+        if( (L = L->next)==NULL) return;  // РѕС€РёР±РєР°
     }
     L_pop_front(L, v);
 }
-void L_popA(list *L, int i, int v) { // вставить число v ПОСЛЕ номера i в списке L
+void L_popA(list *L, int i, int v) { // РІСЃС‚Р°РІРёС‚СЊ С‡РёСЃР»Рѕ v РџРћРЎР›Р• РЅРѕРјРµСЂР° i РІ СЃРїРёСЃРєРµ L
     for( int j=0; j<=i; j++) {
-        if( (L = L->next)==NULL) return;  // ошибка
+        if( (L = L->next)==NULL) return;  // РѕС€РёР±РєР°
     }
     L_pop_front(L, v);
 }
-void L_popE(list *L, int v) { // вставить число v в хвост списка L
+void L_popE(list *L, int v) { // РІСЃС‚Р°РІРёС‚СЊ С‡РёСЃР»Рѕ v РІ С…РІРѕСЃС‚ СЃРїРёСЃРєР° L
     while( L->next != NULL) L = L->next;
     link *L1 = new link;
     L->next = L1;
     L1->id = v;
     L1->next = NULL;
 }
-void L_del1(list *L) { // удалить первый элемент
+void L_del1(list *L) { // СѓРґР°Р»РёС‚СЊ РїРµСЂРІС‹Р№ СЌР»РµРјРµРЅС‚
     link *L1 = L->next;
     L->id = L1->id;
     L->next = L1->next;
     delete L1;
+}
+void L_del(list *L, int i) { // СѓРґР°Р»РёС‚СЊ i-Р№ СЌР»РµРјРµРЅС‚
+    for( int j=0; j<i; j++) {
+        if( (L = L->next)==NULL) return;  // РѕС€РёР±РєР°
+    }
+    L_del1(L);
 }
 void Change(list *L,list *M1,list *M2) {
     int x;
@@ -84,21 +83,72 @@ void Change(list *L,list *M1,list *M2) {
 
     L_del1(M1);
     L_del1(M2);
-
 }
+void menu(){
+}
+//------------------------------------------------------
 int main()
 {
-L_start(&L,rand()%4);
-for(int i=0;i<=20;i++)
-{
-L_pop_front(&L, (i^10-i^19-i)%31-i^(3)-i);
-}
+    //int x,y,i;
+     L_start(&L,rand()%4);
+            for(int i=0;i<=10;i++){
+                L_pop_front(&L, (i^10-i^19-i)%31-i^(3)-i);
+            }
+    L_print(&L);
+    int action;
+    do {
+        printf("\n0: Exit\n"
+               "1: Insert 3 at the begin\n"
+               "2: Insert 4 at the end\n"
+               "3: Insert 5 before the 3rd elem.\n"
+               "4: Delete first element\n"
+               "5: Delete 3rd element\n"
+               "6: ....individual\n>");
+        cin>>action;
+        if(action==0) break;
+        if(action==1) {
+            L_pop_front(&L, 3);
+            L_print(&L);
+            continue;
+        }
+        if(action==2) {
+            L_popE(&L, 4);
+            L_print(&L);
+            continue;
+        }
+        if(action==3) {
+            L_pop(&L, 3, 5);
+            L_print(&L);
+            continue;
+        }
+        if(action==4) {
+            L_del1(&L);
+            L_print(&L);
+            continue;
+        }
+        if(action==5) {
+            L_del(&L, 3);
+            L_print(&L);
+            continue;
+        }
+        if(action==6) {
 
-L_print(&L);
-L_start(&M1,1000);
-L_start(&M2,-1000);
-Change(&L,&M1,&M2);
-L_print(&M1);
-L_print(&M2);
+
+            L_print(&L);
+            L_start(&M1,1000);
+            L_start(&M2,-1000);
+            Change(&L,&M1,&M2);
+            L_print(&M1);
+            L_print(&M2);
+            continue;
+        }
+
+    } while(1);
+
+
+
+
+   
+
 return 0;
 }
